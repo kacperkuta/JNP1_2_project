4410f853483e2a4fa8bc74fad4577b2463a3e3cb
Kacper Kuta <k.kuta2@student.uw.edu.pl>
2019-10-24 12:44:53 +0200

    Wrzucam na wszelki bo testuję, ale to nie jest zbyt dobra wersja

--- JNP1_2_projekt/poset.cpp
+++ JNP1_2_projekt/poset.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <unordered_set>
 #include <set>
-
+#include <queue>
 #include "poset.h"
+#include <cassert>
 
 using relation = std::pair<char const*, std::unordered_set<char const*>>;
 using poset = std::pair<unsigned long, std::set<relation>>;
 using all_posets = std::set<poset>;
 
 all_posets posets;
+unsigned long free_ids_beg = 0;
+std::queue<unsigned long> free_ids;
+
 
 bool poset_exists(unsigned long id) {
     for (auto &p : posets) {
@@ -20,6 +24,44 @@ bool poset_exists(unsigned long id) {
 
 /*Jeżeli w zbiorze posetów wskazywanym przez posets istnieje poset o
   danym przez id identyfikatorze, zwraca true. False wpp.*/
+
+unsigned long choose_new_id() {
+    if (free_ids.empty()) {
+        free_ids_beg++;
+        return free_ids_beg - 1;
+    }
+    unsigned long id = free_ids.front();
+    free_ids.pop();
+    return id;
+}
+
+/* Wybiera id dal nowego posetu spośród wolnych id.*/
+
+unsigned long poset_new() {
+    poset p;
+    unsigned long id = choose_new_id();
+    p.first = id;
+    posets.insert(p);
+    assert(poset_exists(id));
+    return id;
+}
+poset find_poset(unsigned long id) {
+    for (auto itr = posets.begin(); itr != posets.end(); itr++) {
+        if ((*itr).first == id)
+            return *itr;
+    }
+    return *(posets.end());
+}
+
+/*Znajduje poset o danym id, zwraca iterator do posets na niego, albo iterator
+  na koniec posets, gdy nie ma takiego posetu.*/
+
+size_t poset_size(unsigned long id) {
+    if (poset_exists(id)) {
+        poset p = find_poset(id);
+
+    }
+}
 
 
 int main() {
